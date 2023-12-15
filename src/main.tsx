2e889aa7a21fa45af52baa0487906da66f98a111
Svygzhryr <pamixy@gmail.com>
2023-12-15 17:14:28 +0300

    feat: add base layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeOptions, ThemeProvider, createTheme } from '@mui/material/styles';
 
 import './index.scss';
 import App from './app/App';
 
+export const themeOptions: ThemeOptions = {
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          color: '#f9c9d2',
+          fontWeight: '400',
+          fontSize: '24px',
+          borderRadius: '10px',
+          boxShadow: 'none',
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          background: '#6D0F20',
+          borderRadius: '10px',
+
+          '&.Mui-error': {
+            // border: '4px solid red',
+          },
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          '&.Mui-focused': {
+            color: '#f9c9d2',
+          },
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          fontSize: '14px',
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {},
+      },
+    },
+  },
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#6D0F20',
+      dark: '#d01c3c',
+      light: '#df6379',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#150f0f',
+      paper: '#3c2222',
+    },
+    text: {
+      primary: '#f9c9d2',
+      secondary: '#edc6c6',
+      disabled: '#ab9090',
+    },
+    error: {
+      main: '#ffaed1',
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,3 +1,45 @@
+import { Button, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { RouteLinks } from '../../utils/types';
+
+import styles from './SignUp.module.scss';
+
 export const SignUp = () => {
-  return <h1>SignUp</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <h2>Sign Up</h2>
+      <form className={styles.formWrapper}>
+        <TextField
+          // error
+          helperText=" "
+          className={styles.input}
+          label="Email"
+          type="email"
+          autoComplete="current-password"
+        />
+        <TextField
+          // error
+          helperText=" "
+          className={styles.input}
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <TextField
+          // error
+          helperText=" "
+          className={styles.input}
+          label="Confirm Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <Button type="submit" className={styles.button} variant="contained">
+          Sign up
+        </Button>
+      </form>
+      <p>Already have an account?</p>
+      <Link to={RouteLinks.SignIn}>Log in</Link>
+    </div>
+  );
 };
