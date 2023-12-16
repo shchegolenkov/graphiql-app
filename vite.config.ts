cb92587c7df3a765b0f7f9e54b7c48f9ba538d3c
shchegolenkov <denis.amorales@ya.ru>
2023-12-16 23:38:36 +0300

    refactor: add global import vars.scss

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/_vars.scss";`,
+      },
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
