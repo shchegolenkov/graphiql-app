03249b932ad29b792747c4d7ed5f40f31dc520c2
shchegolenkov <denis.amorales@ya.ru>
2023-12-17 10:30:08 +0300

    chore: add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
