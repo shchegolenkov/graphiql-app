5e48d77a9a492bd464dbed13cc7ce3235b66565a
shchegolenkov <denis.amorales@ya.ru>
2023-12-17 22:13:29 +0300

    style: styles naming

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,53 +1,65 @@
 import { Link } from 'react-router-dom';
-import s from './Footer.module.scss';
 import Logo from '../../assets/svg/rsLogo.svg?react';
 import EgorAvatar from '../../assets/images/egorAvatar.jpg';
 import DenisAvatar from '../../assets/images/denisAvatar.jpg';
 import PavelAvatar from '../../assets/images/pavelAvatar.jpg';
+import styles from './Footer.module.scss';
 
 export const Footer = () => {
   return (
-    <footer className={s.footer}>
-      <ul className={s.linksList}>
+    <footer className={styles.footer}>
+      <ul className={styles.linksList}>
         <li>
           <h2>2023</h2>
         </li>
         <li>
-          <div className={s.imageBlock}>
+          <div className={styles.imageBlock}>
             <Link
               to="https://github.com/ygrcore"
               target="_blank"
-              className={s.link}
+              className={styles.link}
             >
-              <img src={EgorAvatar} alt="Egor's Github" className={s.img} />
+              <img
+                src={EgorAvatar}
+                alt="Egor's Github"
+                className={styles.img}
+              />
             </Link>
           </div>
         </li>
         <li>
-          <div className={s.imageBlock}>
+          <div className={styles.imageBlock}>
             <Link
               to="https://github.com/shchegolenkov"
               target="_blank"
-              className={s.link}
+              className={styles.link}
             >
-              <img src={DenisAvatar} alt="Denis' Github" className={s.img} />
+              <img
+                src={DenisAvatar}
+                alt="Denis' Github"
+                className={styles.img}
+              />
             </Link>
           </div>
         </li>
         <li>
-          <div className={s.imageBlock}>
+          <div className={styles.imageBlock}>
             <Link
               to="https://github.com/Svygzhryr"
               target="_blank"
-              className={s.link}
+              className={styles.link}
             >
-              <img src={PavelAvatar} alt="Pavel's Github" className={s.img} />
+              <img
+                src={PavelAvatar}
+                alt="Pavel's Github"
+                className={styles.img}
+              />
             </Link>
           </div>
         </li>
         <li>
           <Link to="https://rs.school/react" target="_blank">
-            <Logo className={s.logo} />
+            <Logo className={styles.logo} />
           </Link>
         </li>
       </ul>
