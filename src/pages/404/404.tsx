9563564b34cd378955a739ece61af0a47583a37e
Svygzhryr <pamixy@gmail.com>
2023-12-17 10:33:45 +0300

    feat: add 404 page

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,3 +1,18 @@
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import styles from './404.module.scss';
+import { RouteLinks } from '../../utils/types';
+
 export const NotFound = () => {
-  return <h1>404</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>404</h1>
+      <h2>Page not found...</h2>
+      <Link to={RouteLinks.Welcome}>
+        <Button variant="contained" className={styles.button}>
+          Go back
+        </Button>
+      </Link>
+    </div>
+  );
 };
