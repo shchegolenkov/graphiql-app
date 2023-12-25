a2bac4ae01e3ca31f3e91ed719c8b37701a644af
Egor <itpd2010@yandex.ru>
2023-12-25 21:15:52 +0100

    feat: add link to the main page on the welcome page for logged-in user

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,12 +1,37 @@
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import { selectUserEmail } from '../../store/user/user.slice';
 import { useAppSelector } from '../../hooks/redux-hook';
 
+import styles from './Welcome.module.scss';
+
 export const Welcome = () => {
   const email = useAppSelector(selectUserEmail);
-  console.log(email);
+
   return (
     <main>
-      <h1>Welcome</h1>
+      <div className={styles.welcome}>
+        <h1>Welcome</h1>
+        {email ? (
+          <div className={styles.welcome__container}>
+            <h2 className={styles.welcome__title}>You`re logged in!</h2>
+            <Link to="/main">
+              <Button
+                type="button"
+                className={styles.welcome__button}
+                variant="contained"
+              >
+                <Typography
+                  sx={{ fontSize: 32 }}
+                  className={styles.welcome__button_title}
+                >
+                  Start GraphQL`ing!
+                </Typography>
+              </Button>
+            </Link>
+          </div>
+        ) : null}
+      </div>
     </main>
   );
 };
