c7b4919934a6ee231f2d66b8756fcf3a85f0d0c1
Egor <itpd2010@yandex.ru>
2023-12-27 15:55:26 +0100

    refactor: named import for protectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,14 +1,12 @@
-import { ReactNode } from 'react';
+import { ReactNode, FC } from 'react';
 import { Navigate } from 'react-router-dom';
+import { RouteLinks } from '../../utils/types';
 
 interface ProtectedRouteProps {
   children: ReactNode;
   user: boolean;
 }
 
-export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-  children,
-  user,
-}) => {
-  return user ? children : <Navigate to="/"></Navigate>;
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({ children, user }) => {
+  return user ? children : <Navigate to={RouteLinks.Welcome}></Navigate>;
 };
