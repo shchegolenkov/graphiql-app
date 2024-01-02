210cd7428054109f36ef910e7d8a2e30ddc7f59e
Svygzhryr <pamixy@gmail.com>
2024-01-02 16:07:52 +0300

    other: remove type assertion

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -105,7 +105,7 @@ export const Main = () => {
   const handleHeadersChange: ChangeEventHandler<HTMLTextAreaElement> = (
     event
   ) => {
-    const textarea = event.target as HTMLTextAreaElement;
+    const textarea = event.target;
     dispatch(setHeaders(textarea.value));
   };
 
