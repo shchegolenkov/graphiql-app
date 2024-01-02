439804d2507732051f4f0aae0a8136a47340d13d
shchegolenkov <denis.amorales@ya.ru>
2024-01-02 23:06:36 +0300

    fix: disable modal for users after logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import { Tooltip } from '@mui/material';
 import clsx from 'clsx';
 
+import { useAppDispatch } from '../../hooks/redux-hook';
 import { useAuth } from '../../hooks/useAuth';
 import { logout } from '../../firebase';
+import { closeModal } from '../../store/modal/modal.slice';
 import { RouteLinks } from '../../utils/types';
 
 import TranslateIcon from '../../assets/svg/translateIcon.svg?react';
@@ -18,12 +20,18 @@ import GraphQLIcon from '../../assets/svg/graphql_svg.svg?react';
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const dispatch = useAppDispatch();
   const { isAuth, email } = useAuth();
-  const [ isScrolled, setIsScrolled ] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleScroll = () => {
     setIsScrolled(window.scrollY > 0);
-  }
+  };
+
+  const handleLogout = async () => {
+    await logout();
+    dispatch(closeModal());
+  };
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -31,11 +39,13 @@ export const Header = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
+
   return (
-    <header className={clsx(styles.header, {
-      [styles.headerScrolled]: isScrolled,
-    })} >
+    <header
+      className={clsx(styles.header, {
+        [styles.headerScrolled]: isScrolled,
+      })}
+    >
       <nav className={styles.nav}>
         <div className={styles.navBlock}>
           <button className={styles.iconBlock}>
@@ -62,7 +72,7 @@ export const Header = () => {
           )}
           {isAuth ? (
             <Tooltip title={`Logout from ${email}`}>
-              <button className={styles.iconBlock} onClick={logout}>
+              <button className={styles.iconBlock} onClick={handleLogout}>
                 <LogoutIcon className={styles.authIcon} />
                 <p>Logout</p>
               </button>
