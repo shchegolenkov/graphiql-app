a8193830f206b7cb23df86d398a43b75e161deb1
shchegolenkov <denis.amorales@ya.ru>
2024-01-04 23:38:53 +0300

    feat: add query variables to store

--- src/store/editor/selectors.ts
+++ src/store/editor/selectors.ts
@@ -9,3 +9,4 @@ export const selectOutput = (state: RootState) => state.editor.output;
 export const selectEndpoint = (state: RootState) => state.editor.endpoint;
 export const selectModal = (state: RootState) => state.editor.isEndpointOpen;
 export const selectHeader = (state: RootState) => state.editor.headers;
+export const selectVariable = (state: RootState) => state.editor.variables;
