475582bc18246d68d041eeb2933db94f406200c2
Egor <itpd2010@yandex.ru>
2024-01-04 12:10:22 +0100

    feat: signin, signup tests

---
+++ src/pages/SignIn/SignIn.test.tsx
@@ -0,0 +1,44 @@
+import { it, expect, beforeEach, vi } from 'vitest';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../../store/store';
+import { RouteLinks } from '../../utils/types';
+import { SignIn } from './Signin';
+
+beforeEach(cleanup);
+
+it('renders SignIn component correctly', () => {
+  const { container } = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <SignIn />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  expect(container.innerHTML).toBeTruthy();
+});
+
+it('submits form with valid data and redirects to Welcome', async () => {
+  const { getByLabelText, getByText } = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <SignIn />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  fireEvent.input(getByLabelText('Email'), {
+    target: { value: 'be@tester.ru' },
+  });
+  fireEvent.input(getByLabelText('Password'), {
+    target: { value: 'Qwert12!' },
+  });
+
+  fireEvent.submit(getByText('Sign in'));
+
+  vi.waitFor(() => {
+    expect(window.location.pathname).toBe(RouteLinks.Main);
+  });
+});
