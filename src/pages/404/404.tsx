33933f7bf01982f1a32317cf3ae3694f5db5403d
shchegolenkov <denis.amorales@ya.ru>
2024-01-07 19:21:58 +0300

    feat: implement not found page localization

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,18 +1,22 @@
-import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+
 import { RouteLinks } from '../../utils/types';
+import { useLanguage } from '../../hooks/useLanguage';
 
 import styles from './404.module.scss';
 
 export const NotFound = () => {
+  const lang = useLanguage();
+
   return (
     <main>
       <div className={styles.wrapper}>
         <h1>404</h1>
-        <h2>Page not found...</h2>
+        <h2>{lang.not_found_text}</h2>
         <Link to={RouteLinks.Welcome}>
           <Button variant="contained" className={styles.button}>
-            Go back
+            {lang.not_found_button}
           </Button>
         </Link>
       </div>
